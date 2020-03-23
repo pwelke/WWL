ef3a5560d1b9d26e749b9d0a659d7722148e5b26
Pascal Welke <welke@uni-bonn.de>
2020-03-23 16:35:28 +0100

    our experiments

---
+++ experiments/TED_experiments.sh
@@ -0,0 +1,7 @@
+python3 main.py --dataset s60to80 --h 3 --crossvalidation --gridsearch --repetitions 5
+python3 main.py --dataset s80to100 --h 3 --crossvalidation --gridsearch --repetitions 5
+python3 main.py --dataset s160to180 --h 3 --crossvalidation --gridsearch --repetitions 5
+python3 main.py --dataset s200to220 --h 3 --crossvalidation --gridsearch --repetitions 5
+python3 main.py --dataset s200to250 --h 3 --crossvalidation --gridsearch --repetitions 5
+python3 main.py --dataset s240to260 --h 3 --crossvalidation --gridsearch --repetitions 5
+

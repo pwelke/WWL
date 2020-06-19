c0448ef0218b6872d92464bdb4f4856030456f29
Pascal Welke <welke@kdml02.iai.uni-bonn.de>
2020-06-19 10:02:35 +0200

    rename new regex versions of synth results parsers to original file name

--- experiments/TED_experiments_Synth_parse.py
+++ experiments/TED_experiments_Synth_parse.py
@@ -1,15 +1,10 @@
 import os
 import numpy as np
+import sys
+import re
 
 if __name__ == '__main__':
-    results = './results/'
-
-    n = 8
-    ee = 1
-    p_noise = 0.01
-    ga = 200
-    cs = [[1, 2], [2, 4], [4, 8], [8, 8]]
-    ps = [0.25, 0.5, 0.75, 1.0]
+    results = sys.argv[1]
 
     f1 = open(os.path.join(results, 'tab_Synth1.csv'), 'w')
     f1.write('Dataset, Accuracy, std\n')
@@ -17,17 +12,25 @@ if __name__ == '__main__':
     f2 = open(os.path.join(results, 'tab_Synth2.csv'), 'w')
     f2.write('n, ee, c_min, c_max, p, p_noise, ga, j, Accuracy, std\n')
 
-    for j in [1, 2, 3, 4, 5]:
-        for c in cs:
-            for p in ps:
-                [c_min, c_max] = c
+    regex = re.compile('sythetic_n(.*?)_ee(.*?)_c(.*?)c(.*?)_p(.*?)_pn(.*?)_ga(.*?)_i(.*?).p')
 
-                db = f'sythetic_n{n}_ee{ee}_c{c_min}c{c_max}_p{p}_pn{p_noise}_ga{ga}_i{j}.p'
-                input_file = os.path.join(results, db, 'results_' + db + '_crossvalidation_gridsearch_all1repetitions.csv')
+    for db in os.listdir(results):
+        match = regex.match(db)
+        if match:
+            n = match.group(1)
+            ee = match.group(2)
+            c_min = match.group(3)
+            c_max = match.group(4)
+            p = match.group(5)
+            p_noise = match.group(6)
+            ga = match.group(7)
+            j = match.group(8)
 
-                accs = np.loadtxt(input_file)
-                f1.write(f'{db}, {np.mean(accs)}, {np.std(accs)}\n')
-                f2.write(f'{n}, {ee}, {c_min}, {c_max}, {p}, {p_noise}, {ga}, {j}, {np.mean(accs)}, {np.std(accs)}\n')
+            input_file = os.path.join(results, db, 'results_' + db + '_crossvalidation_gridsearch_all1repetitions.csv')
+
+            accs = np.loadtxt(input_file)
+            f1.write(f'{db}, {np.mean(accs)}, {np.std(accs)}\n')
+            f2.write(f'{n}, {ee}, {c_min}, {c_max}, {p}, {p_noise}, {ga}, {j}, {np.mean(accs)}, {np.std(accs)}\n')
 
     f1.close()
     f2.close()
--- experiments/TED_experiments_Synth_parse_avg.py
+++ experiments/TED_experiments_Synth_parse_avg.py
@@ -1,26 +1,29 @@
 import os
 import numpy as np
+import re
+import sys
 
 if __name__ == '__main__':
-    results = './results/'
+    results = sys.argv[1]
 
-    n = 8
-    ee = 1
-    p_noise = 0.01
-    ga = 200
-    cs = [[1, 2], [2, 4], [4, 8], [8, 8]]
-    ps = [0.25, 0.5, 0.75, 1.0]
+    f2 = open(os.path.join(results, 'tab_Synth_avg.csv'), 'w')
+    f2.write('n, ee, c_min, c_max, p, p_noise, ga, j, Accuracy, std\n')
 
-    # f1 = open(os.path.join(results, 'tab_Synth1.csv'), 'w')
-    # f1.write('Dataset, Accuracy, std\n')
+    regex = re.compile('sythetic_n(.*?)_ee(.*?)_c(.*?)c(.*?)_p(.*?)_pn(.*?)_ga(.*?)_i(.*?).p')
 
-    f2 = open(os.path.join(results, 'tab_Synth3.csv'), 'w')
-    f2.write('n, ee, c_min, c_max, p, p_noise, ga, Accuracy, std\n')
+    for db in os.listdir(results):
+        match = regex.match(db)
+        if match:
+            n = match.group(1)
+            ee = match.group(2)
+            c_min = match.group(3)
+            c_max = match.group(4)
+            p = match.group(5)
+            p_noise = match.group(6)
+            ga = match.group(7)
+            j = match.group(8)
 
-    for c in cs:
-        for p in ps:
-            [c_min, c_max] = c
-
+            # inefficient. We will copmute the same thing five times. but faster for me, right now.
             accs = list()
             for j in [1, 2, 3, 4, 5]:
                 db = f'sythetic_n{n}_ee{ee}_c{c_min}c{c_max}_p{p}_pn{p_noise}_ga{ga}_i{j}.p'
@@ -29,8 +32,6 @@ if __name__ == '__main__':
                 accs.append(np.loadtxt(input_file))
             allaccs = np.stack(accs)
             allaccs.flatten()
-            # f1.write(f'{db}, {np.mean(allaccs)}, {np.std(allaccs)}\n')
             f2.write(f'{n}, {ee}, {c_min}, {c_max}, {p}, {p_noise}, {ga}, {np.mean(accs)}, {np.std(accs)}\n')
 
-    # f1.close()
     f2.close()

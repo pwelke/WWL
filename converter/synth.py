60c80f1a1afc10e984b3deecf8f01f31b6a063e2
Pascal Welke <welke@kdml02.iai.uni-bonn.de>
2020-06-19 09:38:22 +0200

    make synth converter more generic

--- converter/synth.py
+++ converter/synth.py
@@ -1,31 +1,34 @@
 
 import os
 import pickle
+import sys
 
 import egonetconverter
 
 if __name__ == '__main__':
-    source = '../data/Synth'
-    output = '../data'
+    source = sys.argv[1]
+    output = sys.argv[2]
 
-    n = 8
-    ee = 1
-    p_noise = 0.01
-    ga = 200
-    cs = [[1, 2], [2, 4], [4, 8], [8, 8]]
-    ps = [0.25, 0.5, 0.75, 1.0]
+    # n = 8
+    # ee = 1
+    # p_noise = 0.01
+    # ga = 200
+    # cs = [[1, 2], [2, 4], [4, 8], [8, 8]]
+    # ps = [0.25, 0.5, 0.75, 1.0]
+    #
+    # for j in [1, 2, 3, 4, 5]:
+    #     for c in cs:
+    #         for p in ps:
+    #             [c_min, c_max] = c
+    #
+    #             db = f'sythetic_n{n}_ee{ee}_c{c_min}c{c_max}_p{p}_pn{p_noise}_ga{ga}_i{j}.p'
 
-    for j in [1, 2, 3, 4, 5]:
-        for c in cs:
-            for p in ps:
-                [c_min, c_max] = c
+    for db in os.listdir(source):
+        input_file = os.path.join(source, db)
+        with open(input_file, "rb") as f:
+            [G, y] = pickle.load(f)
+            egonetconverter.write_graph_list(name=db, graph_list=G, data_root=output)
+            egonetconverter.write_label_list(name=db, label_list=y, data_root=output)
 
-                db = f'sythetic_n{n}_ee{ee}_c{c_min}c{c_max}_p{p}_pn{p_noise}_ga{ga}_i{j}.p'
-                input_file = os.path.join(source, db)
-                with open(input_file, "rb") as f:
-                    [G, y] = pickle.load(f)
-                    egonetconverter.write_graph_list(name=db, graph_list=G, data_root=output)
-                    egonetconverter.write_label_list(name=db, label_list=y, data_root=output)
-
-                # lazy persons way to create the bash script file
-                print(f'python3 main.py --dataset {db} --h 3 --crossvalidation --gridsearch --repetitions 1')
+        # # lazy persons way to create the bash script file
+        # print(f'python3 main.py --dataset {db} --h 3 --crossvalidation --gridsearch --repetitions 1')
